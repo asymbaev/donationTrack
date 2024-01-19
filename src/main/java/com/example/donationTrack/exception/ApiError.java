63f32d1d1a724a3fe87e3eb5a48b4d9e06eb497f
Dastan <asymbaevdastan07@gmail.com>
2024-01-18 19:40:38 -0500

    Cmpltd

---
+++ src/main/java/com/example/donationTrack/exception/ApiError.java
@@ -0,0 +1,12 @@
+package com.example.donationTrack.exception;
+
+import java.time.LocalDateTime;
+
+public record ApiError (
+        String path,
+        String message,
+
+int statusCode,
+    LocalDateTime timestamp
+    )
+{}
