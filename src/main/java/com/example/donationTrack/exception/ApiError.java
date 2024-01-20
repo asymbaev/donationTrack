63e1e24f064131a28a176ecf7bb3e72f54c4f469
Dastan <asymbaevdastan07@gmail.com>
2024-01-19 21:24:52 -0500

    Added new importws and working on security

--- src/main/java/com/example/donationTrack/exception/ApiError.java
+++ src/main/java/com/example/donationTrack/exception/ApiError.java
@@ -1,12 +1,12 @@
 package com.example.donationTrack.exception;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public record ApiError (
+public record ApiError(
         String path,
         String message,
-
-int statusCode,
-    LocalDateTime timestamp
-    )
-{}
+        int statusCode,
+        LocalDateTime timestamp
+) {
+}
--- src/main/java/com/example/donationTrack/exception/DefaultExceptionHandler.java
+++ src/main/java/com/example/donationTrack/exception/DefaultExceptionHandler.java
@@ -5,21 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpServerErrorException;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class DefaultExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleException(ResourceNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<ApiError> handleException(ResourceNotFoundException e, HttpServletRequest request){
         ApiError apiError = new ApiError(
                 request.getRequestURI(),
                 e.getMessage(),
                 HttpStatus.NOT_FOUND.value(),
                 LocalDateTime.now()
         );
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
+
     }
 
 }
